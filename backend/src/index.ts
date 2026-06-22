ddb275414ecb5d23d19ade516db779736ac2582f
kithusibrian <briaokm@gmail.com>
2026-06-22 12:39:18 +0300

    remove unused imports and simplify root route handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,21 +2,17 @@ import dotenv from "dotenv";
 dotenv.config({ override: true });
 import passport from "passport";
 import "./config/passport.config";
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import { Env } from "./config/env.config";
 import { initializeCrons } from "./cron";
-import { HTTPSTATUS } from "./config/http.config";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
-import { BadRequestException } from "./utils/app-error";
-import { asyncHandler } from "./middlewares/asyncHandler.middlerware";
 import connctDatabase from "./config/database.config";
 import authRoutes from "./routes/auth.route";
 import { passportAuthenticateJwt } from "./config/passport.config";
 import userRoutes from "./routes/user.route";
 import transactionRoutes from "./routes/transaction.route";
 import reportRoutes from "./routes/report.route";
-import { getDateRange } from "./utils/date";
 import analyticsRoutes from "./routes/analytics.route";
 import budgetRoutes from "./routes/budget.route";
 import goalRoutes from "./routes/goal.route";
@@ -36,15 +32,11 @@ app.use(
   }),
 );
 
-app.get(
-  "/",
-  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    throw new BadRequestException("This is a test error");
-    res.status(HTTPSTATUS.OK).json({
-      message: "Hello Welcome brother",
-    });
-  }),
-);
+app.get("/", (_req, res) => {
+  res.status(200).json({
+    message: "Foretrack API is running",
+  });
+});
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
 
